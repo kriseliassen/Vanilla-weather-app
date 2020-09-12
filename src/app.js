7a97f5a836e41df6743609945ff955ece4b3d1f5
Kriskriskris <kristin.eliassen@gmail.com>
2020-09-12 15:24:56 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -44,8 +44,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "3f134ca2a6c6070d8c666bc9f960b659";
-let city = "Stavanger";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "3f134ca2a6c6070d8c666bc9f960b659";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Stavanger");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
